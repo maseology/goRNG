c88b257bd84863b92b9633eb5575bcd4b65ba4e5
maseology <maseology@pm.me>
2023-04-27 17:45:23 -0400

    added qprng (not working)

---
+++ qrng/qrng.go
@@ -0,0 +1,236 @@
+/*
+	MODIFIED FROM: https://github.com/salviati/go-qrand
+
+	Copyright (c) Utkan Güngördü <utkan@freeconsole.org>
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as
+	published by the Free Software Foundation; either version 3 or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details
+
+	You should have received a copy of the GNU General Public
+	License along with this program; if not, write to the
+	Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+package qrng
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"sync"
+)
+
+const (
+	Host = "random.irb.hr"
+	Port = "8000" // "1227"
+)
+
+var response = []string{
+	"OK",
+	"Service was shutting down",
+	"Server was/is experiencing internal errors",
+	"Service said we have requested some unsupported operation",
+	"Service said we sent an ill-formed request packet",
+	"Service said we were sending our request too slow",
+	"Authentication failed",
+	"User quota exceeded",
+}
+
+var remedy = []string{
+	"None",
+	"Try again later",
+	"Try again later",
+	"Upgrade your client software",
+	"Upgrade your client software",
+	"Check your network connection",
+	"Check your login credentials",
+	"Try again later, or contact service admin to increase your quota(s)",
+}
+
+type QRNG struct {
+	buf        *bufio.Reader
+	user, pass string
+	sync.Mutex
+}
+
+// Read requests len(rand) bytes from QRNG server.
+// This function does not user the buffer and
+// always creates a new connection.
+// Try not to use this function —the function
+// you're looking for is probably ReadBytes().
+func (q *QRNG) Read(rand []byte) (int, error) {
+	if len(rand) == 0 {
+		return 0, nil
+	}
+
+	c, err := net.Dial("tcp", net.JoinHostPort(Host, Port))
+	if err != nil {
+		return 0, err
+	}
+	defer c.Close()
+
+	b := bytes.NewBuffer([]byte(""))
+	fmt.Fprintf(b, "%c", 0)
+	binary.Write(b, binary.BigEndian, uint16(len(q.user)+len(q.pass)+6))
+	fmt.Fprintf(b, "%c%s%c%s", len(q.user), q.user, len(q.pass), q.pass)
+	binary.Write(b, binary.BigEndian, uint32(len(rand)))
+	_, err = c.Write(b.Bytes())
+	if err != nil {
+		return 0, err
+	}
+
+	msg := make([]byte, 6)
+
+	_, err = c.Read(msg)
+	if err != nil {
+		return 0, err
+	}
+	b = bytes.NewBuffer(msg)
+	var remedyCode, responseCode uint8
+	binary.Read(b, binary.BigEndian, &responseCode)
+	binary.Read(b, binary.BigEndian, &remedyCode)
+
+	if responseCode != 0 || remedyCode != 0 {
+		var resp, rem string
+
+		if int(responseCode) < len(response) {
+			resp = response[responseCode]
+		} else {
+			resp = fmt.Sprint("Unknown response code ", responseCode)
+		}
+
+		if int(remedyCode) < len(remedy) {
+			rem = remedy[remedyCode]
+		} else {
+			resp = fmt.Sprint("Unknown remedy code ", remedyCode)
+		}
+
+		return 0, errors.New("QRNG: " + resp + ": " + rem)
+	}
+
+	var available uint32
+	binary.Read(b, binary.BigEndian, &available)
+	_, err = c.Read(rand[:available])
+	return int(available), err
+}
+
+// ReadBytes tries to read len(b) bytes of data into b.
+// It returns the number of bytes actually read, which can be less than len(b).
+// An error is returned if fewer bytes are read.
+func (q *QRNG) ReadBytes(p []byte) (int, error) {
+	q.Lock()
+	defer q.Unlock()
+	return io.ReadFull(q.buf, p)
+}
+
+func (q *QRNG) readInto(v interface{}) error {
+	n := 0
+
+	switch t := v.(type) {
+	case *uint8, *int8:
+		n = 1
+	case *uint16, *int16:
+		n = 2
+	case *uint32, *int32, *float32:
+		n = 4
+	case *uint64, *int64, *float64:
+		n = 8
+	default:
+		return errors.New(fmt.Sprint("QRNG.readInto: unexpected data type ", t))
+	}
+
+	rand := make([]byte, n)
+	if _, err := q.ReadBytes(rand); err != nil {
+		return err
+	}
+
+	return binary.Read(bytes.NewBuffer(rand), binary.BigEndian, v)
+}
+
+// Uint8 fetches 8-bit random data and returns it as uint8.
+func (q *QRNG) Uint8() (r uint8, err error) {
+	err = q.readInto(&r)
+	return
+}
+
+// Int8 fetches 8-bit random data and returns it as int8.
+func (q *QRNG) Int8() (r int8, err error) {
+	err = q.readInto(&r)
+	return
+}
+
+// Uint16 fetches 16-bit random data and returns it as uint16.
+func (q *QRNG) Uint16() (r uint16, err error) {
+	err = q.readInto(&r)
+	return
+}
+
+// Int16 fetches 16-bit random data and returns it as int16.
+func (q *QRNG) Int16() (r int16, err error) {
+	err = q.readInto(&r)
+	return
+}
+
+// Uint32 fetches 32-bit random data and returns it as uint32.
+func (q *QRNG) Uint32() (r uint32, err error) {
+	err = q.readInto(&r)
+	return
+}
+
+// Int32 fetches 32-bit random data and returns it as int32.
+func (q *QRNG) Int32() (r int32, err error) {
+	err = q.readInto(&r)
+	return
+}
+
+// Uint64 fetches 64-bit random data and returns it as uint64.
+func (q *QRNG) Uint64() (r uint64, err error) {
+	err = q.readInto(&r)
+	return
+}
+
+// Int64 fetches 64-bit random data and returns it as int64.
+func (q *QRNG) Int64() (r int64, err error) {
+	err = q.readInto(&r)
+	return
+}
+
+// Float32 fetches 32-bit random data and returns it as a float32 in [0.0,1.0)
+func (q *QRNG) Float32() (float32, error) {
+	var r uint32
+	err := q.readInto(&r)
+	return float32(r) / (1 << 32), err
+}
+
+// Float64 fetches 64-bit random data and returns it as a float64 in [0.0,1.0)
+func (q *QRNG) Float64() (float64, error) {
+	var r uint64
+	err := q.readInto(&r)
+	return float64(r) / (1 << 64), err
+}
+
+// New creates a new instances of Quantum Random Bit Generator client.
+// The client-side should have a username and password from the relevant web-site.
+func New(user, pass string, buffersize int) (*QRNG, error) {
+	if buffersize < 1 {
+		return nil, errors.New("QRNG: buffersize is too small")
+	}
+
+	q := &QRNG{user: user, pass: pass}
+	buf := bufio.NewReaderSize(q, buffersize)
+
+	q.buf = buf
+	return q, nil
+}
--- readme.md
+++ readme.md
@@ -1,8 +1,20 @@
-# go pnrg
+# goRNG
+
+go implementation of random number generators.
+
+## Pseudo-random number generators (PRNG)
 
 An implementation of the 32-, and 63-bit L'Ecuyer's (1999) pseudo-random number generators.
 
 **Please note** that design of this package follows the work completed by Jochen Voss on his 64bit Mersenne Twister PRNG, see https://github.com/seehuhn/mt19937
+
+
+## Quantum random number generator (QRNG)
+
+Go client for a 64-bit quantum random number generator service offered by [random.irb.hr](random.irb.hr). *Modified from [https://github.com/salviati/go-qrand](https://github.com/salviati/go-qrand)*. Accesses *"True"* random numbers generated using quantum processes. Requires a username and password.
+
+> We use 'Quantum Random Bit Generator' (QRBG121), which is a fast non-deterministic random bit (number) generator whose randomness relies on intrinsic randomness of the quantum physical process of photonic emission in semiconductors and subsequent detection by photoelectric effect. In this process photons are detected at random, one by one independently of each other. Timing information of detected photons is used to generate random binary digits - bits. The unique feature of this method is that it uses only one photon detector to produce both zeros and ones which results in a very small bias and high immunity to components variation and aging. Furthermore, detection of individual photons is made by a photomultiplier (PMT). Compared to solid state photon detectors the PMT's have drastically superior signal to noise performance and much lower probability of appearing of afterpulses which could be a source of unwanted correlations. Read more at the product page: [http://qrbg.irb.hr/](http://qrbg.irb.hr/). <br> (*from [random.irb.hr](random.irb.hr)*) 
+
 
 ## References
 
